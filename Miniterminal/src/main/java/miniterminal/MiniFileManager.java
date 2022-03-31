7a34d9396a951070f832989a5eaee0995b312936
DAW <DAW@DESKTOP-LLGQQR2>
2022-03-31 17:08:55 +0200

    31/03/2022

--- Miniterminal/src/main/java/miniterminal/MiniFileManager.java
+++ Miniterminal/src/main/java/miniterminal/MiniFileManager.java
@@ -1,3 +1,5 @@
+
+   
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,6 +11,124 @@ package miniterminal;
  *
  * @author DAW
  */
-class MiniFileManager {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+/**
+ *
+ * @author DAW
+ */
+public class MiniFileManager {
+
+    File rutaActual = new File("");
+//Constructor 
+    public MiniFileManager(File ruta) {
+        this.rutaActual = ruta;
+    }
+//Getters & Setters
+    public File getRuta() {
+        return rutaActual;
+    }
+
+    public void setRuta(File ruta) {
+        this.rutaActual = ruta;
+    }
+// Método para que te la ruta completa en la que estamos 
+    public String getPWD() throws IOException {
+
+        String ruta = this.rutaActual.getAbsolutePath();//creamos un Srint de ruta a la cual le metemos ruta que hemos creado 
+        System.out.println(ruta);
+        return ruta;
+    }
+//Método para que que nos movamos de directorio.
+    boolean changeDir(String ruta) throws FileNotFoundException {
+        File nuevaruta = new File("C:\\Users\\DAW\\Desktop\\EjercioA3\\Libros");
+        if (!nuevaruta.exists()) {
+            throw new FileNotFoundException();
+        }
+        rutaActual = nuevaruta;
+        System.out.println("Se ha cambiado de directorio");
+        return true;
+    }
+
+ //Método para imprimir la lista de directorios y archivos de la carpeta actual 
+    void printList(boolean info) throws FileNotFoundException {
+        try {
+            String ruta = rutaActual.getAbsolutePath();// creamos "ruta" para almacenar la "rutaActual"
+            System.out.println(ruta);
+            File carpeta = new File(ruta); //Creamos un objeto "carpeta" en el cual almacenamos "ruta"
+            if (!carpeta.exists()) { //Comprobamos que exista el File "carpeta" si no existe mandamos un Exception 
+                throw new FileNotFoundException();
+            }
+            if (carpeta.isFile()) {   //Si la el objeto "carpeta"  es un File le decimos que lo imprima y nos muestre el nombre
+                System.out.println("" + carpeta.getName());
+            }
+             if (carpeta.isDirectory()) {   //Por otra parte si es un Directory  
+                System.out.println("Nombre del directorio donde nos encontramos: " + carpeta.getName()); 
+                for (File ruta2 : carpeta.listFiles()) {  //Creamos un "ruta2" para almacenar la lista de las carpetas dentro de la ruta 
+                    if (ruta2.isDirectory()) {
+                        System.out.println("Directorio: " + ruta2.getName());
+                    }
+
+                }
+               for (File ruta3 : carpeta.listFiles()) {
+                    if (ruta3.isFile()) {
+                        System.out.println("Fichero: " + ruta3.getName());
+                    }
+
+                }
+             }
+            
+        }catch (Exception e) {
+         System.out.println("No hay elemenmtos dentro de la ruta actual");  }
+    }
+}
+
+/*
+public static String getPWD (String PWD) throws IOException{
+ try {
+     File rutaActual = new File(""); 
+     PWD=rutaActual.getParent();
+ }catch(Exception e){
+     System.out.println("La rutaActual no es valida, pruebe a intrioducir otra rutaActual");
+ }
+ return PWD;
+}
+}
+/**public boolean changeDir (String dir) {
+   try  {
+       File rutaActual = new File();
+       
+   }catch{
+   }
+   
+}
     
 }
+    
+    /*archivos = carpeta.listFiles(filtro);
+if (archivos == null || archivos.length == 0) {
+    System.out.println("No hay elementos dentro de la carpeta actual");
+    return;
+}
+else {
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    for (int i=0; i< archivos.length; i++) {
+        File archivo = archivos[i];
+        System.out.println(String.format("%s (%s) - %d - %s", 
+                        archivo.getName(),
+                        archivo.isDirectory() ? "Carpeta" : "Archivo",
+                        archivo.length(),
+                        sdf.format(archivo.lastModified())
+                ));
+    }
+}
+    return ;
+}
+    
+}*/
--- Miniterminal/src/main/java/miniterminal/MiniTerminal.java
+++ Miniterminal/src/main/java/miniterminal/MiniTerminal.java
@@ -6,6 +6,7 @@
 package miniterminal;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -15,40 +16,42 @@ import java.util.Scanner;
  */
 public class MiniTerminal {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //ruta 
 
         Scanner entrada = new Scanner(System.in);
-        //File ruta = new File("C:\\Users\\DAW\\Desktop\\EjercioA3");
-        MiniFileManager r = new MiniFileManager("C:\\Users\\DAW\\Desktop\\EjercioA3");
-        File ruta = new File("C:\\Users\\DAW\\Desktop\\EjercioA3");
+        File ruta = new File("C:\\Users\\DAW\\Desktop\\");
+        MiniFileManager gerente = new MiniFileManager(ruta);
         boolean salir = false;
-        int menu;
+        //int menu;
 
         System.out.println("Bienvenido al MINITERMINAL");
+       
+        gerente.getPWD();
+        gerente.printList(salir);
 
-        while (!salir) {
-
+       /** while (!salir) {
             System.out.println("Seleccione una opción");
-            System.out.println("     1. PWD, para mostrar la carpeta actual");
+            System.out.println("     1. PWD, para mostrar la ruta");
             System.out.println("     2. CD<DIR>, cambiar la carpeta actual a a DIR Con .. Cambia la la carpeta superior");
-            System.out.println("     3. ls");
+            System.out.println("     3. ls");   
             System.out.println("     4. ll");
             System.out.println("     5. mkdir<DIR>");
             System.out.println("     6. rm<FILE>");
             System.out.println("     7. MV<FILE><FILE2>");
             System.out.println("     8. help");
             System.out.println("     9. exit");
-
             menu = entrada.nextInt();
-
             switch (menu) {
                 case 1:
-
                     System.out.println("A elegido 1.PWD para visualizar en que carpeta esta");
-
-                    ruta
+                    
+                {
+                    String PWD;
+                    r.getPWD(PWD);
+                }
                    
+                  
                     break;
                 case 2:
                     break;
@@ -57,10 +60,8 @@ public class MiniTerminal {
                 case 4:
                     break;
                 case 5:
-
                     break;
                 case 6:
-
                     break;
                 case 7:
                     System.out.println("Movimientos");
@@ -76,6 +77,6 @@ public class MiniTerminal {
                     System.out.println("       Soloopciones entre 1 y 8");
                     System.out.println("--------------------------------------------");
             }
-        }
+        }*/
     }
 }
